1e819e7defb41d4fa0a4eaf79ce856248ffbd7d5
emersonpessoa01 <eps011108@gmail.com>
2020-09-22 16:05:31 -0300

    insert input focus

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,6 +8,10 @@ export default class Header extends Component {
   //     const newText = evt.target.value
   //     this.props.onChangeFilter(newText)
   // };
+
+  componentDidMount() {
+    this.textInput.focus();
+  }
   render() {
     const {
       filter,
@@ -16,20 +20,18 @@ export default class Header extends Component {
       onChangeFilter,
     } = this.props;
     return (
-
-      
       <div className={css.flexRow}>
         <input
           className={css.centeredTitle}
           placeholder="País"
           type="text"
+          ref={(element) => (this.textInput = element)}
           value={filter}
           onChange={onChangeFilter}
         />{" "}
         <br></br>{" "}
         <span className={css.countries}>
-          {countryCount > 1 ? "Países" : "País"}:{" "}
-          <bold>{countryCount}</bold>
+          {countryCount > 1 ? "Países" : "País"}: <bold>{countryCount}</bold>
         </span>
         <span className={css.separator}>|</span>
         <span className={css.population}>
